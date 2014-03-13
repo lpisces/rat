1235fa2626b84674f11b0935d47b0872c47f398a
ruby <ruby@localhost.localdomain>
2014-03-13 15:31:39 +0800

    rat for stock

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rat::Application.config.secret_key_base = '91b3fc0a86488459195c0d51e74dc63c4c6c14d963c42c0ec01dac09ea58135a8fc7d664c94bbff39258d942e3e3a682004ce7d77d38e17c7316d525ec2f2bf5'
